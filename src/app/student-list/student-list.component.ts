dd91d1fc96206aee8d3e0a473b53b105bc18c7ca
Buddhika Walpita19 <buddhisanje@gmail.com>
2024-12-09 10:24:53 +0530

    Add  ondestroy hook

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,161 +1,192 @@
-
-import { Component,  inject,  OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule, DATE_PIPE_DEFAULT_OPTIONS, formatDate } from '@angular/common';
-import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import {
+  CommonModule,
+  DATE_PIPE_DEFAULT_OPTIONS,
+  formatDate,
+} from '@angular/common';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { DataServiceService } from '../data-service.service';
 // import { DataTablesModule } from 'angular-datatables';
 // import DataTables, { Config } from 'datatables.net';
-import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
-import {MatSort, MatSortModule} from '@angular/material/sort';
-import {MatTable, MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
 
 import { Student } from '../../Models/Student';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { StudentDetailsComponent } from '../student-details/student-details.component';
 import { StudentviewdialogComponent } from '../studentviewdialog/studentviewdialog.component';
-
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,RouterOutlet,FormsModule,ReactiveFormsModule,CommonModule, MatPaginatorModule,MatSortModule,MatTable,MatFormFieldModule,MatTableModule,MatInputModule,MatIconModule,MatButtonModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTable,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './student-list.component.html',
-  styleUrl: './student-list.component.css'
+  styleUrl: './student-list.component.css',
 })
-export class StudentListComponent implements OnInit {
-
+export class StudentListComponent implements OnInit, OnDestroy {
   dialog = inject(MatDialog);
 
-  students:Student[]=[];
+  students: Student[] = [];
   dataSource: MatTableDataSource<Student>;
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
   @ViewChild(MatSort)
   sort!: MatSort;
-  displayedColumns = ['studentCode', 'firstName', 'lastName', 'mobile','email','NIC','edit', 'delete','view'];
+  private subscription?: Subscription;
+  displayedColumns = [
+    'studentCode',
+    'firstName',
+    'lastName',
+    'mobile',
+    'email',
+    'NIC',
+    'edit',
+    'delete',
+    'view',
+  ];
 
-  constructor(private datservice:DataServiceService, private router : Router,private dialogRef: MatDialogRef<StudentviewdialogComponent>){
+  constructor(
+    private datservice: DataServiceService,
+    private router: Router,
+    private dialogRef: MatDialogRef<StudentviewdialogComponent>
+  ) {
     //const students: Student[] = [];
-    
-      this.getStudents();
+
+    this.getStudents();
 
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(this.students);
-  
-  
-    }
-    openTaskDialog(id: any,stucode:string) {
-   
-debugger;
-this.loadData(id,stucode);
-       this.datservice.url='https://localhost:7120/api/StudentApi';
-   
-  
-      
-    }
-  
-    loadData(id: any,StudentCode: string)
- {
-
-  debugger;
-  // var id = parseInt(this.route.snapshot.paramMap.get('id'));
-   this.datservice.getStudentbyId(id,StudentCode).subscribe({
-    next:(value)=>{
-      debugger;
-    this.students=value;
-     console.log(this.students);
-     let dialogRef =this.dialog.open(StudentviewdialogComponent, {
-      data: [this.students],
-      height: '400px',
-      width: '500px',
-    });
-  
-
   }
-  });
-
-     }
-   
-    public deleteStudents(id: any,index: any,StudentCode:string)
-    {
-      debugger;
-      if(confirm("Are you sure to delete?")) {
-      this.datservice.deleteStudents(id,index,StudentCode).subscribe((data:any)=>{
-        debugger;
-  
-    alert("Record Succesfully deleted");
-  this.datservice.url='https://localhost:7120/api/StudentApi'
-   // this.router.navigate(['/contactList']);
-   this.students.splice(index,1);
-   this.Navigate(1);
-   this.setDataSource();
-    });
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
-  
-    }
+  openTaskDialog(id: any, stucode: string) {
+    debugger;
+    this.loadData(id, stucode);
+    this.datservice.url = 'https://localhost:7120/api/StudentApi';
+  }
 
-    Navigate(type: number ) {
-      if(type==1)
-      {
-        this.router.navigate(['/StudentList']);
-      }
-        else if(type==2)
-        {
-          this.router.navigate(['/Student']);
-        }
-    
-      }
-  
+  loadData(id: any, StudentCode: string) {
+    debugger;
+    // var id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.subscription = this.datservice
+      .getStudentbyId(id, StudentCode)
+      .subscribe({
+        next: (value) => {
+          debugger;
+          this.students = value;
+          console.log(this.students);
+          let dialogRef = this.dialog.open(StudentviewdialogComponent, {
+            data: [this.students],
+            height: '400px',
+            width: '500px',
+          });
+        },
+      });
+  }
 
-    ngAfterViewInit() {
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+  public deleteStudents(id: any, index: any, StudentCode: string) {
+    debugger;
+    if (confirm('Are you sure to delete?')) {
+      this.subscription = this.datservice
+        .deleteStudents(id, index, StudentCode)
+        .subscribe((data: any) => {
+          debugger;
+
+          alert('Record Succesfully deleted');
+          this.datservice.url = 'https://localhost:7120/api/StudentApi';
+          // this.router.navigate(['/contactList']);
+          this.students.splice(index, 1);
+          this.Navigate(1);
+          this.setDataSource();
+        });
     }
-  
-    applyFilter(filterValue: string) {
-      filterValue = filterValue.trim(); // Remove whitespace
-      filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-      this.dataSource.filter = filterValue;
+  }
+
+  Navigate(type: number) {
+    if (type == 1) {
+      this.router.navigate(['/StudentList']);
+    } else if (type == 2) {
+      this.router.navigate(['/Student']);
     }
-  setDataSource(){
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+  }
+  setDataSource() {
     this.dataSource = new MatTableDataSource(this.students);
-    this.dataSource.filterPredicate = function(data, filter: string): boolean {
-     return data.firstName.toLowerCase().includes(filter) || data.lastName.toLowerCase().includes(filter) || data.mobile.includes(filter)
-     || data.studentCode.includes(filter) || data.email.includes(filter) ;
-   };
+    this.dataSource.filterPredicate = function (data, filter: string): boolean {
+      return (
+        data.firstName.toLowerCase().includes(filter) ||
+        data.lastName.toLowerCase().includes(filter) ||
+        data.mobile.includes(filter) ||
+        data.studentCode.includes(filter) ||
+        data.email.includes(filter)
+      );
+    };
   }
-    getStudents(){
-      debugger;
+  getStudents() {
+    debugger;
 
-    this.datservice.getStudents().subscribe({
-          next:(value)=>{
-            debugger;
-        
-        this.students=value;
-         console.log(this.students);
-         this.setDataSource();
-      
-     
-          }     
+    this.subscription = this.datservice.getStudents().subscribe({
+      next: (value) => {
+        debugger;
 
+        this.students = value;
+        console.log(this.students);
+        this.setDataSource();
+      },
     });
-    }
-    ngOnInit(): void {
-      
-          // Assign the data to the data source for the table to render
-         
-    }
-
-
-   
-
-
-    
+  }
+  ngOnInit(): void {
+    // Assign the data to the data source for the table to render
+  }
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,171 +1,173 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule, formatDate } from '@angular/common';
-import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { DataServiceService } from '../data-service.service';
-
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive,RouterOutlet,FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './student.component.html',
-  styleUrl: './student.component.css'
+  styleUrl: './student.component.css',
 })
-export class StudentComponent implements OnInit {
+export class StudentComponent implements OnInit, OnDestroy {
+  isFormSubmitted: boolean = false;
+  studentdata: any;
+  httpClient = inject(HttpClient);
+  id: any;
+  stucode: any;
+  private subscription?: Subscription;
 
-isFormSubmitted:boolean=false;
-studentdata:any;
-httpClient=inject(HttpClient);
- id: any;
- stucode:any;
-
-constructor(private datservice:DataServiceService, private router : Router,private route: ActivatedRoute){
- // this.url='https://localhost:44351/api/Contact';
-
-}
-
-
-StudentForm=new FormGroup(
-  {
-studentcode:new FormControl("",[Validators.required]),
-firstName:new FormControl("",[Validators.required]),
-lastName:new FormControl("",[Validators.required,Validators.minLength(4)]),
-mobile:new FormControl("",[Validators.required]),
-email:new FormControl("",[Validators.required,Validators.email]),
-NIC:new FormControl("",[Validators.required]),
-Dob:new FormControl(""),
-address:new FormControl("")
-
-  })
-
-
-
-
-ngOnInit():void{
- // this.fetchContacts();
- // this.onSubmit();
-
-this.id=this.route.snapshot.params['Id'];
-this.stucode= this.route.snapshot.params['StudentCode'];
- //var id = parseInt(this.route.snapshot.paramMap.get('id'));
- if(this.id!=undefined)
- {
-
- this.loadData(this.id,this.stucode);
- }
-}
-
-
-loadData(id: any,StudentCode: string)
- {
-
-  // var id = parseInt(this.route.snapshot.paramMap.get('id'));
-   this.datservice.getStudentbyId(id,StudentCode).subscribe({
-    next:(value)=>{
-      
-    this.studentdata=value;
-     console.log(this.studentdata);
-   var dobdt=  formatDate(value.dob,'yyyy-MM-dd',"en-US");
-     this.StudentForm.setValue({
-       studentcode: value.studentCode,
-       firstName: value.firstName,
-       lastName: value.lastName,
-       mobile: value.mobile,
-       email: value.email,
-       NIC: value.nic,    
-       Dob: dobdt,
-       address: value.address
-     });
-    
-
+  constructor(
+    private datservice: DataServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    // this.url='https://localhost:44351/api/Contact';
   }
+
+  StudentForm = new FormGroup({
+    studentcode: new FormControl('', [Validators.required]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    mobile: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    NIC: new FormControl('', [Validators.required]),
+    Dob: new FormControl(''),
+    address: new FormControl(''),
   });
 
-     }
+  ngOnInit(): void {
+    // this.fetchContacts();
+    // this.onSubmit();
 
-  
-
-    // }
-
-onClear():void{
-  this.StudentForm=new FormGroup(
-    {
-      studentcode:new FormControl(""),
-      firstName:new FormControl(""),
-      lastName:new FormControl(""),
-      mobile:new FormControl(""),
-      email:new FormControl(""),
-      NIC:new FormControl(""),
-      Dob:new FormControl(""),
-      address:new FormControl("")
-  
-    });
-}
-onNavigate()
-{
-  this.router.navigate(['/StudentList'])
-  .then(() => {
-    window.location.reload();
-  });
-}
-onFormSubmit(){
-  this.id=this.id===undefined?"00000000-0000-0000-0000-000000000000":this.id;
-  
-  
-  const addStudentRequest={
-    Id:this.id,
-    studentcode:this.StudentForm.value.studentcode,
-    firstname:this.StudentForm.value.firstName,
-    lastName:this.StudentForm.value.lastName,
-    mobile:this.StudentForm.value.mobile,
-    email:this.StudentForm.value.email,
-    NIC:this.StudentForm.value.NIC,
-    Dob:this.StudentForm.value.Dob==''?formatDate(new Date(),'yyyy-MM-dd',"en-US"):this.StudentForm.value.Dob,
-    address:this.StudentForm.value.address
-
+    this.id = this.route.snapshot.params['Id'];
+    this.stucode = this.route.snapshot.params['StudentCode'];
+    //var id = parseInt(this.route.snapshot.paramMap.get('id'));
+    if (this.id != undefined) {
+      this.loadData(this.id, this.stucode);
+    }
   }
-  const isFormValid=this.StudentForm.valid;
 
-  this.isFormSubmitted=true;
-  this.StudentForm.markAsTouched();
-  if(isFormValid)
-{
-debugger;
-  if(this.id=="00000000-0000-0000-0000-000000000000")
-  {
-  
-  this.datservice.addStudent(addStudentRequest).subscribe((data:any)=>{
-   
-  this.router.navigate(['/StudentList'])
-  .then(() => {
-    window.location.reload();
-  });
-
-alert("Record Succesfully Saved");
-});
-  }
-  else{
-    debugger;
-    this.datservice.updateStudents(this.id,this.stucode,addStudentRequest).subscribe((data:any)=>{
-   
-      this.router.navigate(['/StudentList'])
-      .then(() => {
-        window.location.reload();
+  loadData(id: any, StudentCode: string) {
+    // var id = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.subscription = this.datservice
+      .getStudentbyId(id, StudentCode)
+      .subscribe({
+        next: (value) => {
+          this.studentdata = value;
+          console.log(this.studentdata);
+          var dobdt = formatDate(value.dob, 'yyyy-MM-dd', 'en-US');
+          this.StudentForm.setValue({
+            studentcode: value.studentCode,
+            firstName: value.firstName,
+            lastName: value.lastName,
+            mobile: value.mobile,
+            email: value.email,
+            NIC: value.nic,
+            Dob: dobdt,
+            address: value.address,
+          });
+        },
       });
-    
-    alert("Record Succesfully Updated");
-  })
+  }
 
-  
+  // }
 
-}
-}
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+  onClear(): void {
+    this.StudentForm = new FormGroup({
+      studentcode: new FormControl(''),
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      mobile: new FormControl(''),
+      email: new FormControl(''),
+      NIC: new FormControl(''),
+      Dob: new FormControl(''),
+      address: new FormControl(''),
+    });
+  }
+  onNavigate() {
+    this.router.navigate(['/StudentList']).then(() => {
+      window.location.reload();
+    });
+  }
+  onFormSubmit() {
+    this.id =
+      this.id === undefined ? '00000000-0000-0000-0000-000000000000' : this.id;
 
-}
-  
+    const addStudentRequest = {
+      Id: this.id,
+      studentcode: this.StudentForm.value.studentcode,
+      firstname: this.StudentForm.value.firstName,
+      lastName: this.StudentForm.value.lastName,
+      mobile: this.StudentForm.value.mobile,
+      email: this.StudentForm.value.email,
+      NIC: this.StudentForm.value.NIC,
+      Dob:
+        this.StudentForm.value.Dob == ''
+          ? formatDate(new Date(), 'yyyy-MM-dd', 'en-US')
+          : this.StudentForm.value.Dob,
+      address: this.StudentForm.value.address,
+    };
+    const isFormValid = this.StudentForm.valid;
 
+    this.isFormSubmitted = true;
+    this.StudentForm.markAsTouched();
+    if (isFormValid) {
+      debugger;
+      if (this.id == '00000000-0000-0000-0000-000000000000') {
+        this.subscription = this.datservice
+          .addStudent(addStudentRequest)
+          .subscribe((data: any) => {
+            this.router.navigate(['/StudentList']).then(() => {
+              window.location.reload();
+            });
 
+            alert('Record Succesfully Saved');
+          });
+      } else {
+        debugger;
+        this.subscription = this.datservice
+          .updateStudents(this.id, this.stucode, addStudentRequest)
+          .subscribe((data: any) => {
+            this.router.navigate(['/StudentList']).then(() => {
+              window.location.reload();
+            });
+
+            alert('Record Succesfully Updated');
+          });
+      }
+    }
+  }
 }
